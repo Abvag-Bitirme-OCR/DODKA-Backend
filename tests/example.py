4df7d2b056985eac7ac695b1e6b15edc22aeddae
Melih Selami Urkmez (Dogus Teknoloji) <melih.urkmez@d-teknoloji.com.tr>
2023-02-22 13:09:30 +0300

    ci: Example

--- tests/example.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from API import app
-
-client = TestClient(app)
-
-def test_read_main():
-    response = client.get("/healthcheck")
-    assert response.status_code == 500
-    assert response.json() == {"message": "Hello World"}
